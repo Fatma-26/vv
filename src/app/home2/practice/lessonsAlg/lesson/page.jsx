8066e720c1cf5676e81b95935326cf0f79c3e6b3
Fatma-26 <yasmeenyahya26@gmail.com>
2024-09-25 16:57:15 +0300

    تحديث

--- src/app/home2/practice/lessonsAlg/lesson/page.jsx
+++ src/app/home2/practice/lessonsAlg/lesson/page.jsx
@@ -1,45 +1,7 @@
-/* 
-"use client";
-import React from 'react';
-
-export default function CodeBlock() {
-  const codeCpp = `
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}`;
-
-  return (
-    <div className="ec">
-      <div className="black">
-        <pre>
-          <code>
-            {codeCpp}
-          </code>
-        </pre>
-      </div>
-      <div className="content">
-      
-        <p className="q">Question</p>
-        <p className="o">The output is:</p>
-       <div className='d'><button className="ebtn">45</button>
-        <button className="ebtn">485</button>
-        <button className="ebtn">25</button>
-        <button className="ebtn">00</button> </div> 
-        <div id="timer" >50</div>
-
-      </div>
-    </div>
-  );
-} */
 "use client";
 import React, { useState, useEffect } from 'react';
 
 export default function CodeBlock() {
-  // الكود المطلوب عرضه
   const codeCpp = `
 #include <iostream>
 
@@ -48,10 +10,8 @@ int main() {
     return 0;
 }`;
 
-  // حالة المؤقت
   const [timer, setTimer] = useState(50);
 
-  // تقليل المؤقت كل ثانية
   useEffect(() => {
     const countdown = setInterval(() => {
       if (timer > 0) {
@@ -61,10 +21,7 @@ int main() {
     return () => clearInterval(countdown);
   }, [timer]);
 
-  // عند الضغط على زر
- /*  const handleClick = (value) => {
-    console.log(`Button ${value} clicked!`);
-  }; */
+
 
   return (
     <div className="ec">
